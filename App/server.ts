0208058ee57bd4a3d8de2bb1b5c947ba3b80c286
David Conley <dconley1989@gmail.com>
2022-08-10 15:21:33 -0600

    added a new file to handle the login requests and added the json object from express to be able to pull down info from the req.body object

--- App/server.ts
+++ App/server.ts
@@ -5,11 +5,9 @@ dotenv.config({ path: __dirname + "/.env" });
 
 const server: Application = express();
 
-const Port = process.env.Port || 9000;
+server.use(express.json);
 
-server.get("/", (req: Request, res: Response): void => {
-  res.send("Hello Typescript with Node.js");
-});
+const Port = process.env.Port || 9000;
 
 server.listen(Port, (): void => {
   console.log("Server is listening");
