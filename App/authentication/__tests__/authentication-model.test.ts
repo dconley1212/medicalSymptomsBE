51dff04fbb577247b7f3ec8cd08c29abc71bfa29
David Conley <dconley1989@gmail.com>
2022-08-31 16:08:22 -0600

    added couple tests. One to test the getUsesById and the other test was created to stop any asynchronous operations after the tests completed. The later test still needs some work

--- App/authentication/__tests__/authentication-model.test.ts
+++ App/authentication/__tests__/authentication-model.test.ts
@@ -1,7 +1,16 @@
-import { isJSDocNonNullableType } from "typescript";
-import { getUserByFilter } from "../authentication-model";
+import { getUserByFilter, getUserByID } from "../authentication-model";
+
+afterAll((done) => {
+  done();
+});
 
 describe("authentication db acess functions", () => {
+  test("getUserById", async () => {
+    const userId = 1;
+    const [user] = await getUserByID(userId);
+    expect(user.username).toBe("jonny");
+  });
+
   test("the getUserByFilter", async () => {
     const username = "jonny";
     const [user] = await getUserByFilter(username);
