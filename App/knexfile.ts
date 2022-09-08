d575cd00a251ac75b35533b3f7e5498dd4aea72b
David Conley <dconley1989@gmail.com>
2022-09-08 16:07:16 -0600

    fixed the knexfile to have the config to have the ability to test the models for the database

--- App/knexfile.ts
+++ App/knexfile.ts
@@ -1,12 +1,31 @@
 import type { Knex } from "knex";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + "/.env" });
 
 module.exports = {
+  testing: {
+    client: "pg",
+    connection: {
+      database: "postgres_tests",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    useNullAsDefault: true,
+  },
   development: {
     client: "pg",
     connection: {
       database: "postgres",
-      user: "postgres",
-      password: "AmyandDave101918",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
