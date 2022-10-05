3c2a98ee794e8b2d246b9c34a3a88102c2b7397f
David Conley <dconley1989@gmail.com>
2022-10-05 17:55:03 -0600

    made some changes on the backend with the post request by adding error handling and the try catch method. And made some changes to the user account modle file but then realized those changes need to wait because I need to rollback the last migration

--- App/user-account/user-account-model.ts
+++ App/user-account/user-account-model.ts
@@ -1,5 +1,13 @@
 import { db } from "../database/db-config";
 
+// there is a problem when I send a post request with the insert model
+// I believe it is because the data I am sending over doesn't fill all
+// has more values than columns in userAddresssInfo table and the
+// foreign key column was never created beacause I set it up incorrectly
+// so I as a next step I need to rollback the last migration and edit
+// the create table function to be able to modify the table to represent
+// what I am sending over in the post request.
+
 export interface UserAddress {
   username: string;
   phone: string;
@@ -10,10 +18,10 @@ export interface UserAddress {
   city: string;
   state: string;
   zipcode: string;
-  id: string;
 }
 
 export const addUserAddressInfo = async (userInfo: UserAddress) => {
+  console.log(userInfo);
   const [userAddressInfo] = await db("userAddressInfo").insert(userInfo, [
     "username",
     "phone",
@@ -24,8 +32,6 @@ export const addUserAddressInfo = async (userInfo: UserAddress) => {
     "city",
     "state",
     "zipcode",
-    "id",
   ]);
-
   return userAddressInfo;
 };
--- App/user-account/user-account.ts
+++ App/user-account/user-account.ts
@@ -2,6 +2,10 @@ import { Router, Request, Response, NextFunction } from "express";
 import { validateToken } from "../reviews/reviews-middleware";
 import { addUserAddressInfo, UserAddress } from "./user-account-model";
 
+// left off trying to post to the userAddressInfo table and got an error when
+// inserting the data. The data shows in the req body, but I need to fix the insert
+// function to the databse
+
 const router = Router();
 
 router.get(
@@ -14,34 +18,40 @@ router.post(
   "/:id/address",
   validateToken,
   async (req: Request, res: Response, next: NextFunction) => {
-    const {
-      username,
-      phone,
-      firstName,
-      lastName,
-      address,
-      apartment_suite_etc,
-      city,
-      state,
-      zipcode,
-    } = req.body;
+    try {
+      const {
+        username,
+        phone,
+        firstName,
+        lastName,
+        address,
+        apartment_suite_etc,
+        city,
+        state,
+        zipcode,
+      } = req.body;
 
-    const { id } = req.params;
+      const { id } = req.params;
 
-    const userInfo: UserAddress = {
-      username,
-      phone,
-      firstName,
-      lastName,
-      address,
-      apartment_suite_etc,
-      city,
-      state,
-      zipcode,
-      id,
-    };
-    const userAddressInfo = await addUserAddressInfo(userInfo);
-    res.status(200).json(userAddressInfo);
+      console.log(id);
+
+      const userInfo: UserAddress = {
+        username,
+        phone,
+        firstName,
+        lastName,
+        address,
+        apartment_suite_etc,
+        city,
+        state,
+        zipcode,
+        id,
+      };
+      const userAddressInfo = await addUserAddressInfo(userInfo);
+      res.status(200).json(userAddressInfo);
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
